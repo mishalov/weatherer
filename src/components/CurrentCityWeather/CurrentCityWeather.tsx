4fd1407ef045ff11b0c210812e95efde7117811c
Mishalov Evgenii <forwow495800@ya.ru>
2021-06-27 16:25:18 +0200

    Divide pages into Main and ByCity

--- src/components/CurrentCityWeather/CurrentCityWeather.tsx
+++ src/components/CurrentCityWeather/CurrentCityWeather.tsx
@@ -3,6 +3,7 @@ import styles from "./CurrentCityWeather.module.scss";
 import Image from "next/image";
 import buildWeatherIconUrl from "utils/buildWeatherIconUrl";
 import TemperatureLabel from "components/TemperatureLabel";
+import cn from "classnames";
 
 interface ICurrentCityWeatherProps {
   weather: IWeather;
@@ -14,8 +15,23 @@ const CurrentCityWeather: React.FC<ICurrentCityWeatherProps> = ({
   const { weather: weatherInfo } = weather;
   return (
     <article className={styles.currentWeather}>
-      <h1 className={styles.title}>{weather.name}: </h1>
-      <h2 className={styles.description}>{weatherInfo.summary.description}</h2>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>{weather.name}: </h1>
+        <h2 className={styles.description}>
+          {weatherInfo.summary.description}
+        </h2>
+      </div>
+
+      <div className={cn(styles.temperature, styles.temperature_actual)}>
+        <div className={styles.temperature_label}>actual</div>
+        <TemperatureLabel value={weatherInfo.temperature.actual} sign="°C" />
+      </div>
+
+      <div className={cn(styles.temperature, styles.temperature_feelsLike)}>
+        <div className={styles.temperature_label}>feels like</div>
+        <TemperatureLabel value={weatherInfo.temperature.feelsLike} sign="°C" />
+      </div>
+
       <div className={styles.icon_container}>
         <Image
           src={buildWeatherIconUrl(weatherInfo.summary.icon)}
@@ -24,11 +40,6 @@ const CurrentCityWeather: React.FC<ICurrentCityWeatherProps> = ({
           alt={weatherInfo.summary.title}
         />
       </div>
-      <TemperatureLabel
-        value={weatherInfo.temperature.actual}
-        sign="°C"
-        className={styles.temperature}
-      />
     </article>
   );
 };
