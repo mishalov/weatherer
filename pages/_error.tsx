fb1a6f4956a38de66eafb3c2bdc96a1958af8022
evgenii <forwow495800@ya.ru>
2021-07-12 10:57:42 +0200

    Add params to 404

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,5 +1,13 @@
+import { GetServerSideProps } from "next";
 import React from "react";
 
-const Error = () => <div>Error</div>;
+const Error = ({ statusCode }: { statusCode: string }) => (
+  <div>{statusCode}</div>
+);
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const { statusCode } = res;
+  return { props: { statusCode } };
+};
 
 export default Error;
